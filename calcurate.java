8fa60526b391d3e04e35d55d70eb1a5f7b3e5f14
Eunjae Lee <dmswo1925@naver.com>
2022-05-08 00:19:18 +0900

    calculrate

--- calcurate.java
+++ calcurate.java
@@ -8,11 +8,11 @@ public class calcurate {
 		
 		Scanner sc = new Scanner(System.in);
 		String error = " ";
-		System.out.println("1. +\t2. -\t3. *\t4. /\t5. exit");
+		System.out.println("11. +\t2. -\t3. *\t4. /\t5. exit");
 		int i=sc.nextInt();
 		
-		while (i<0 || i>=0) {
-			cal(i);
+		while (true) {
+			i = cal(i); 
 			if (i==5) {
 				error = "수고하셨습니다!";
 				System.out.println(error);
@@ -28,41 +28,39 @@ public class calcurate {
 	
 	
 	
-	public static void cal(int i){
+	public static int cal(int i){ 
 		Scanner sc = new Scanner(System.in);
-		int a;	int b;
+		double a;	double b;
+		
 			while(0<i && i<=4) {
+				double result= 0;
 				if(i==1) {
 					System.out.println("더할 두 숫자를 입력해주세요");
 					a = sc.nextInt();
 					b = sc.nextInt();
-					int add = a+b;
-					System.out.println("결과 : " + add);
-				}
-				if(i==2) {
+					result = a+b;
+				}else if(i==2) {
 					System.out.println("뺄 두 숫자를 입력해주세요");
 					a = sc.nextInt();
 					b = sc.nextInt();
-					int sub = a-b;
-					System.out.println("결과 : " + sub);
-				}
-				if(i==3) {
+					result = a-b;
+				}else if(i==3) {
 					System.out.println("곱할 두 숫자를 입력해주세요");
 					a = sc.nextInt();
 					b = sc.nextInt();
-					int gup = a*b;
-					System.out.println("결과 : " + gup);
-				}
-				if(i==4) {
+					result = a*b;
+				}else if(i==4) {
 					System.out.println("나눌 두 숫자를 입력해주세요");
 					a = sc.nextInt();
 					b = sc.nextInt();
-					double div = (double)a/b;
-					System.out.println("결과 : " + div);
+					result = a/b;
 				}
+				System.out.println("결과 : " + result);
 				System.out.println("목록\n1. +\t2. -\t3. *\t4. /\t5. exit");
 				i = sc.nextInt();
 			}
+			
+		return i;
 	}
 
 }
